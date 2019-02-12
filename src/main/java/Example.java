ea08066c158c4ea455abc816b12cfb4df36bf050
ncutLJD <962603220@qq.com>
2019-02-12 23:13:34 +0800

    main描述

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 /**
- * @description: @RestController是一个原型注解，它给阅读Spring编写代码提供了提示，提示类扮演特定的角色
+ * @description:
+ * - @RestController是一个原型注解，它给阅读Spring编写代码提供了提示，提示类扮演特定的角色
  * 在本例中，我们的类是一个@Controller，因此Spring在处理传入的web请求时考虑它。
  *
  * - @RequestMapping注释提供“路由”信息。它告诉Spring任何带有 / path的HTTP请求都应该映射到home方法。
@@ -19,6 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * - @EnableAutoConfiguration 注解告诉SpringBoot根据你添加的jar依赖项“猜测”你希望如何配置Spring
  * 由于Spring-boot-starter-web添加了Tomcat和Spring MVC, 因此自动配置假定你正在开发web应用程序，并且相应地设置Spring
+ *
+ * 应用程序的最后一部分是主方法，这只是遵循应用程序入口点Java约定的标准方法。我们的主要方法通过调用run委托给Spring Boot的
+ * SpringApplication类。SpringApplication引导我们的应用程序启动Spring，而Spring又启动自动配置的Tomcat web服务器。我们需要将Example.class
+ * 作为参数传递给run方法，以告诉SpringApplication哪个是主要的Spring组件。args数组还可以用于公开任何命令行参数。
  * */
 @RestController
 @EnableAutoConfiguration
